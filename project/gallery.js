e86fa21610eaa0f2db8cabcaa3be6a51987084a4
rlatp1409 <stanley710@naver.com>
2021-08-22 23:39:09 +0900

    gallery finish

--- project/gallery.js
+++ project/gallery.js
@@ -1,15 +1,5 @@
 //Activate Slicker
-$(document).ready(function(){
-  $('.slider').slick({
-      dots : true,
-      infinite : true,
-      autoplay : true,
-      autoplaySpeed : 2600,
-      speed: 500,
-      fade: true,
-      cssEase: 'linear',
-    });
-  });
+
 
 
 
