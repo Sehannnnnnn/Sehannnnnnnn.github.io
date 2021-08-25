b9ba46fcb296cd68d1808943201f6affb27dc0ab
rlatp1409 <stanley710@naver.com>
2021-08-25 19:43:59 +0900

    gallery article

--- project/gallery.js
+++ project/gallery.js
@@ -1,4 +1,21 @@
-//Activate Slicker
+var _item = getElementsByTagName('img');
+_item.addEventListener('mouseover',function(){
+    var div = document.createElement('div');
+    div.createTextNode('클릭하여 기사 원문 보러가기');
+    _item.appendChild(div)
+});
+
+
+//navigation hover 애니메이션
+var nav = document.getElementsByTagName('li');
+
+nav.addEventListener('mouseover', function() {
+    nav.setAttribute('class','hover');
+});
+
+nav.addEventListener('mouseout',function(){
+    nav.removeAttribute('class');
+});
 
 
 
